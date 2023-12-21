d197df33cb06014103eb7ae664164896f2bfdd3a
Купцов Андрей Евгеньевич <a.kupcov@univ.priv>
2023-12-21 15:35:38 +0300

    переделал

--- 12ex.py
+++ 12ex.py
@@ -1,33 +1,32 @@
-def geometric_progression_nth_term(a, r, n):
-    if n == 1:
-        return a
-    else:
-        return r * geometric_progression_nth_term(a, r, n - 1)
+def geomet_progres(n, b, g):
+    return geomet_progres(n - 1, b, g) * g if n > 1 else b
 
-def geometric_progression_sum(a, r, n):
-    if n == 1:
-        return a
-    else:
-        return a * (1 - r**n) / (1 - r)
 
-def arithmetic_progression_nth_term(a, d, n):
-    if n == 1:
-        return a
-    else:
-        return a + (n - 1) * d
+def sum_geomet_progres(n, b, g):
+    return sum_geomet_progres(n - 1, b, g) + b * g ** (n - 1) if n > 1 else b
 
-def decimal_to_binary(n):
-    if n == 0:
-        return ""
-    else:
-        return decimal_to_binary(n // 2) + str(n % 2)
+
+def alg_progres(n, a, k):
+    return alg_progres(n - 1, a, k) + k if n > 1 else a
+
+
+def bin_x(a):
+    return bin_x(a // 2) + [a % 2] if a else []
+
 
 def gcd(a, b):
-    if b == 0:
-        return a
-    else:
-        return gcd(b, a % b)
+    return gcd(b, a % b) if b else a
+
 
 def lcm(a, b):
-    return abs(a * b) // gcd(a, b)
+    return a // gcd(a, b) * b
 
+
+try:
+    print(geomet_progres(3, 2, 2))
+    print(sum_geomet_progres(3, 2, 2))
+    print(alg_progres(3, 2, 2))
+    print(bin_x(9))
+    print(lcm(28, 56))
+except ValueError:
+    print("ERROR")
